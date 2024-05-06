0a0a28f3253846b470c9571f5265603751fe1da7
Francis Wedemeyer <francisdayagro24@gmail.com>
2024-05-07 00:37:35 +0800

    functional landscape config

--- app/src/main/java/com/example/appdev_finalproject/MainActivity.java
+++ app/src/main/java/com/example/appdev_finalproject/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.appdev_finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.Configuration;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in_langscape);
+        setContentView(R.layout.activity_sign_in);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_sign_in_langscape);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_sign_in_langscape);
+        } else {
+            setContentView(R.layout.activity_sign_in);
+        }
     }
 }
