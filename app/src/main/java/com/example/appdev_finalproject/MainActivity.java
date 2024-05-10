84f3571179319389c65db97485ab41598e1566c6
Francis Wedemeyer <francisdayagro24@gmail.com>
2024-05-11 02:18:43 +0800

    added progress bar and gif in intro

--- app/src/main/java/com/example/appdev_finalproject/MainActivity.java
+++ app/src/main/java/com/example/appdev_finalproject/MainActivity.java
@@ -1,26 +1,30 @@
 package com.example.appdev_finalproject;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends Base {
+
+    private static final int DELAY_MILLIS = 3000; // 5 seconds delay
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setupListeners();
-    }
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
 
-    private void setupListeners() {
-        View constraintLayout = findViewById(R.id.startclick);
-
-        constraintLayout.setOnClickListener(new View.OnClickListener() {
+        // Delay transition to IntroSlideActivity after 5 seconds
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
+            public void run() {
                 startActivity(new Intent(MainActivity.this, IntroSlideActivity.class));
+                finish(); // Finish MainActivity to prevent going back to it
             }
-        });
+        }, DELAY_MILLIS);
     }
 }
