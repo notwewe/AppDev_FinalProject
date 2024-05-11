ac31c0e112466c9c0bf9f610099ee565e609c800
hulyarein <julialaine.segundo@cit.edu>
2024-05-11 17:36:10 +0800

    SignUp backend

--- app/src/main/java/com/example/appdev_finalproject/SignUp.java
+++ app/src/main/java/com/example/appdev_finalproject/SignUp.java
@@ -5,9 +5,20 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends Base {
+
+    EditText signupName, signupStudID, signupEmail, signupPass;
+    Button signupBtn;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,34 @@ public class SignUp extends Base {
         } else {
             setContentView(R.layout.activity_sign_up);
         }
+
+
+        signupName = findViewById(R.id.nameEditText);
+        signupStudID = findViewById(R.id.studentIDEditText);
+        signupEmail = findViewById(R.id.emailEditText);
+        signupPass = findViewById(R.id.passwordEditText);
+        signupBtn = findViewById(R.id.signUpButton);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("users");
+
+                String name = signupName.getText().toString();
+                String studID = signupStudID.getText().toString();
+                String email = signupEmail.getText().toString();
+                String pass = signupPass.getText().toString();
+
+                UserHelper UH = new UserHelper(name, studID, email, pass);
+                reference.child(name).setValue(UH);
+
+                Toast.makeText(SignUp.this, "You have sign up successfully!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignUp.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
+
 
         TextView loginText = findViewById(R.id.LoginText);
         loginText.setOnClickListener(new View.OnClickListener() {
